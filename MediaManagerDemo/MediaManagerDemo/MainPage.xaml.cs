4fe8466ab306857ab4d21d322405efef939dc8a8
Allan Pead <adpead@gmail.com>
2021-01-07 16:02:20 +0200

    Initial commit

---
+++ MediaManagerDemo/MediaManagerDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MediaManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MediaManagerDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CrossMediaManager.Current.Play("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3");
+        }
+    }
+}
